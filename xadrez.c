988f1d5fa9fd6349314640ccc6b97b0feafc243e
Kleverson Oliveira <oliveira.kleverson0@gmail.com>
2025-08-16 19:14:03 +0000

    NV. MESTRE

--- xadrez.c
+++ xadrez.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
 
+// Função recursiva para mover a Torre
+void anda_Torre(int i){
+    if (i > 0) {
+        
+        printf("Direita\n");
+        anda_Torre(i - 1);
+}
+}
+
+// Função recursiva para mover o Bispo
+void anda_Bispo(int i){
+    if (i > 0) {
+        anda_Bispo(i - 1);
+        printf("Cima, Direita\n");
+}
+}
+
+// Função recursiva para mover a Rainha
+void anda_Rainha(int i){
+    if (i > 0) {
+        printf("Esquerda\n");
+        anda_Rainha(i - 1);
+}
+}
+
+// Função recursiva para mover o Cavalo
+void anda_Cavalo(int i){
+    if (i > 0) {
+        printf("Cima\n");
+        anda_Cavalo(i - 1);
+        if(i == 1) {
+            printf("Direita\n");
+        }
+       
+}
+}
+
 int main() {
     // Simulação do movimento da Torre (usando for) 
     // A Torre se move 5 casas para a direita.
     printf(" Movimento da Torre \n");
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n");
-    }
-
-   
+    anda_Torre(5);
     printf("\n"); 
 
     //  Simulação do movimento do Bispo (usando while) 
     // O Bispo se move 5 casas na diagonal (cima e direita).
     printf(" Movimento do Bispo \n");
-    int casas_bispo = 5;
-    while (casas_bispo > 0) {
-        printf("Cima, Direita\n");
-        casas_bispo--;
-    }
+    anda_Bispo(5);
 
     
     printf("\n"); // Adiciona uma linha em branco para separar os movimentos
@@ -26,27 +55,15 @@ int main() {
     //  Simulação do movimento da Rainha (usando do-while) 
     // A Rainha se move 8 casas para a esquerda.
     printf(" Movimento da Rainha \n");
-    int casas_rainha = 8;
-        do {
-            printf("Esquerda\n");
-            casas_rainha--;
-        } while (casas_rainha > 0);
+    anda_Rainha(8);
    
 printf("\n"); 
 
     //  Simulação do movimento do cavalo (usando for e while) 
     // o cavalo se move 2 casas para baixo e uma para esquerda.
     printf(" Movimento do Cavalo \n");
-        for(int i = 0; i <= 1; i++ ){
-
-            while (i < 2)
-            {
-                printf("Baixo\n");
-                i++;
-            };
-            printf("Esquerda\n");
+    anda_Cavalo(2);
             
-        }
 
     return 0;
 }
